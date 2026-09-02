4ca4b512911d6e0e3bffe023416c29301cc213cf
Falconleos <leonelfsotopublic@gmail.com>
2026-09-02 20:23:50 -0300

    funcion add

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,16 @@
 const mainObject = document.querySelector('main');
+const btnAdd = document.querySelector('.bookAdd form button');
+    const inTitle = document.querySelector('#title');   
+    const inAuthor = document.querySelector('#author');   
+    const inGenre = document.querySelector('#genre');   
+    const inYear = document.querySelector('#year');   
 
+
+btnAdd.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    const title = inTitle.value;
+    const author = inAuthor.value;
+    const genre = inGenre.value;
+    const year = inYear.value;
+})
